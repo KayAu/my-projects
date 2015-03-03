f48bb04e39ef9edcfe464a682e5fec50082fd744
KayAu <kayeau80@gmail.com>
2015-03-03 17:23:58 +0800

    3 Mar 2015 Changes

--- GreatSavings/Controllers/ProductController.cs
+++ GreatSavings/Controllers/ProductController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.Entity;
 namespace GreatSavings.Controllers
 {
     public class ProductController : Controller
