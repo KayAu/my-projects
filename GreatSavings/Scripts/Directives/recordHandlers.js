9f691c72f675f5ed3f6ca3cbc361398f667ca6c9
KayAu <kayeau80@gmail.com>
2015-04-19 00:26:55 +0800

    18 Apr - Part 4 by KY

---
+++ GreatSavings/Scripts/Directives/recordHandlers.js
@@ -0,0 +1,15 @@
+﻿bizModule.directive('whenScrolled', function () {
+
+    return function (scope, element, attrs) {
+        angular.element($window).bind("scroll", function () {
+            if (this.pageYOffset >= 100) {
+                scope.boolChangeClass = true;
+                console.log('Scrolled below header.');
+            } else {
+                scope.boolChangeClass = false;
+                console.log('Header is in view.');
+            }
+            scope.$apply(attr.whenScrolled);
+        });
+    };
+});
